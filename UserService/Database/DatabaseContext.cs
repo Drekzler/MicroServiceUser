cb4add4a781106a596a10396ebee1cac99d4beab
jonhei13 <jonhei13@ru.is>
2020-08-16 21:50:01 +0000

    microserviceProject

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;initial catalog=UserMicroServices;persist security info=True; user id=UserAdmin; password=$super123212");
+        }
+    }
+}
